6887bed61b82efb97864c622d1873cc205bd9a7c
Alex <alexgmark@gmail.com>
2025-12-15 23:37:12 +0000

    Installing analytics-admin to fix missing redirect script

--- resources/js/pages/ga4/index.tsx
+++ resources/js/pages/ga4/index.tsx
@@ -11,7 +11,7 @@ import {
 } from '@/components/ui/card';
 import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem } from '@/types';
-import { Form, Head, Link, usePage } from '@inertiajs/react';
+import { Form, Head, usePage } from '@inertiajs/react';
 import {
     AlertCircle,
     BarChart3,
@@ -119,7 +119,9 @@ function DisconnectedState() {
             </CardContent>
             <CardFooter>
                 <Button asChild className="w-full">
-                    <Link href={GA4Controller.connect().url}>
+                    {/* Replacing <Link> with <a> to fix localhost CORS issues */}
+                    {/* <Link href={GA4Controller.connect().url}> */}
+                    <a href={GA4Controller.connect().url}>
                         <svg
                             className="mr-2 size-5"
                             viewBox="0 0 24 24"
@@ -131,7 +133,7 @@ function DisconnectedState() {
                             <path d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z" />
                         </svg>
                         Connect with Google
-                    </Link>
+                    </a>
                 </Button>
             </CardFooter>
         </Card>
